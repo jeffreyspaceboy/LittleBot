7ab9548cebc9e512746aeec1b3db339a6c7c999e
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2021-12-08 09:24:46 -0800

    Adding Drivetrain

---
+++ include/Drivetrain.h
@@ -0,0 +1,35 @@
+/*---DRIVETRAIN_H---*/
+#ifndef DRIVETRAIN_H
+#define DRIVETRAIN_H
+/*----------------------------------------------------------------------------*/
+/*    Module:       Drivetrain.h                                              */
+/*    Author:       Jeffrey Fisher II                                         */
+/*    Created:      2021-12-08                                                */
+/*----------------------------------------------------------------------------*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Definitions.h"
+#include "Motor.h"
+#include "Encoder.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct{
+    char name[NAME_MAX_SIZE];
+    Motor *left_motor, *right_motor;
+} Drivetrain;
+
+Drivetrain drivetrain_init(char drivetrain_name[NAME_MAX_SIZE], Motor *left_motor, Motor *right_motor);
+int drivetrain_del(Motor *drivetrain);
+
+int drivetrain_spin(Motor *drivetrain, int leftpower);
+int drivetrain_stop(Motor *drivetrain);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+/*---DRIVETRAIN_H---*/
