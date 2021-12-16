351a75e6fce9ea03199a304255e3db31efe6b744
jeffreyspaceboy <jeffreyspaceboy@gmail.com>
2021-12-16 08:32:16 +0000

    Main clean up

--- main.c
+++ main.c
@@ -34,33 +34,28 @@ void  signal_handler(int signal){ RUNNING = false; }
 
 int main(int argc, char * argv[]){
     if (gpioInitialise() < 0) { return FAILURE; }
-    printf("\n\n===STARTING=ROBOT===\n\n");
+    signal(SIGINT, signal_handler);
+    printf("\n===STARTING=ROBOT===\n");
     int STATUS = 0;
 
     /*---ROBOT-CODE-HERE---\/---*/
-    Encoder_t left_encoder = encoder_init("LEFT_ENCODER", L_ENC_A, L_ENC_B,  1/(44.0*21.3), false);
-    PID_Controller_t pid_velocity_left = pid_init(3.5F, 0.00001F, 0.0001F);
-    Motor_t left_motor = motor_init("LEFT_MOTOR", L_MTR_EN, L_MTR_A, L_MTR_B, true, &left_encoder, &pid_velocity_left);
-
-    Encoder_t right_encoder = encoder_init("RIGHT_ENCODER", R_ENC_A, R_ENC_B,  1/(44.0*21.3), true);
-    PID_Controller_t pid_velocity_right = pid_init(3.5F, 0.00001F, 0.0001F);
-    Motor_t right_motor = motor_init("RIGHT_MOTOR", R_MTR_EN, R_MTR_A, R_MTR_B, false, &right_encoder, &pid_velocity_right);
-    
-    Drivetrain_t drivetrain = drivetrain_init("DRIVETRAIN", &left_motor, &right_motor);
-    
+    float ratio = 1.0F/(44.0F * 21.3F);
+    PID_Controller_t l_vel_pid = pid_init(3.5F, 0.00001F, 0.0001F);
+    PID_Controller_t r_vel_pid = pid_init(3.5F, 0.00001F, 0.0001F);
+    Encoder_t l_encoder = encoder_init("LEFT_ENCODER", L_ENC_A, L_ENC_B,  ratio, false);
+    Encoder_t r_encoder = encoder_init("RIGHT_ENCODER", R_ENC_A, R_ENC_B,  ratio, true);
+    Motor_t l_motor = motor_init("LEFT_MOTOR", L_MTR_EN, L_MTR_A, L_MTR_B, true, &l_encoder, &l_vel_pid);
+    Motor_t r_motor = motor_init("RIGHT_MOTOR", R_MTR_EN, R_MTR_A, R_MTR_B, false, &r_encoder, &r_vel_pid);
+    Drivetrain_t drivetrain = drivetrain_init("DRIVETRAIN", &l_motor, &r_motor);
     /*---ROBOT-CODE-HERE---/\---*/
     
     float rpm_target = 60.0F;
-    float left_power, right_power;
     while(RUNNING){
-        signal(SIGINT, signal_handler);
-
         /*---ROBOT-CODE-HERE---\/---*/
-        drivetrain__set_rpm(&drivetrain, rpm_target, rpm_target);
-        printf("L:(%f|%d) R:(%f|%d)\n", motor_get_rpm(&left_motor), left_motor.power, motor_get_rpm(&right_motor), right_motor.power);
+        STATUS |= drivetrain__set_rpm(&drivetrain, rpm_target, rpm_target);
+        printf("L:(%f|%d) R:(%f|%d)\n", motor_get_rpm(&l_motor), l_motor.power, motor_get_rpm(&r_motor), r_motor.power);
         gpioSleep(PI_TIME_RELATIVE, 0, 50000);
         /*---ROBOT-CODE-HERE---/\---*/
-        
         if(STATUS != SUCCESS){ break; }
     }
 
