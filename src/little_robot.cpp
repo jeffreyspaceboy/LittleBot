5358287626b4a552c1524f93ad623a152d177c58
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2022-02-22 20:47:45 -0800

    Pre-Restructure

--- src/little_robot.cpp
+++ src/little_robot.cpp
@@ -1,81 +1,81 @@
 
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_action/rclcpp_action.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <geometry_msgs/msg/pose.hpp>
+// #include <rclcpp/rclcpp.hpp>
+// #include <rclcpp_action/rclcpp_action.hpp>
+// #include <geometry_msgs/msg/twist.hpp>
+// #include <geometry_msgs/msg/pose.hpp>
 
-//---PKG-LIBS---//
-#include "little_bot/action/rotate_absolute.hpp"
+// //---PKG-LIBS---//
+// #include "little_bot/action/rotate_absolute.hpp"
 
-#include <math.h>
+// #include <math.h>
 
-class Little_Robot: public rclcpp::Node{
-    public:
-        using RotateAbsoluteGoalHandle = rclcpp_action::ServerGoalHandle<little_bot::action::RotateAbsolute>;
+// class Little_Robot: public rclcpp::Node{
+//     public:
+//         using RotateAbsoluteGoalHandle = rclcpp_action::ServerGoalHandle<little_bot::action::RotateAbsolute>;
 
-        Little_Robot(): Node("little_bot"){
-            this->declare_parameter("start_position", std::vector<double>{0.0, 0.0, 0.0});
-            this->declare_parameter("start_orientation", std::vector<double>{0.0, 0.0, 0.0, 1.0});
-            velocity_subr_ = this->create_subscription<geometry_msgs::msg::Twist>("command_velocity", 10, std::bind(&Little_Robot::velocityCallback, this, std::placeholders::_1));
-            std::vector<double> start_position, start_orientation;
-            this->get_parameter("start_position", start_position);
-            this->get_parameter("start_orientation", start_orientation);
-            pose_.position.set__x(start_position[0]);
-            pose_.position.set__y(start_position[1]);
-            pose_.position.set__z(start_position[2]);
-            pose_.orientation.set__x(start_orientation[0]);
-            pose_.orientation.set__y(start_orientation[1]);
-            pose_.orientation.set__z(start_orientation[2]);
-            pose_.orientation.set__w(start_orientation[3]);
-            RCLCPP_INFO(this->get_logger(), "POSE: Position: (x: %0.3f, y: %0.3f, z: %0.3f) ", start_position[0], start_position[1], start_position[2]);
-            RCLCPP_INFO(this->get_logger(), "POSE: Orientation: (x: %0.3f, y: %0.3f, z: %0.3f, w: %0.3f) ", start_orientation[0], start_orientation[1], start_orientation[2], start_orientation[3]);
-        }
-    private:
-        geometry_msgs::msg::Pose pose_;
-        geometry_msgs::msg::Twist twist_;
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr velocity_subr_;
-        rclcpp_action::Server<little_bot::action::RotateAbsolute>::SharedPtr rotate_absolute_action_server_;
-        std::shared_ptr<RotateAbsoluteGoalHandle> rotate_absolute_goal_handle_;
-        std::shared_ptr<little_bot::action::RotateAbsolute::Feedback> rotate_absolute_feedback_;
-        std::shared_ptr<little_bot::action::RotateAbsolute::Result> rotate_absolute_result_;
+//         Little_Robot(): Node("little_bot"){
+//             this->declare_parameter("start_position", std::vector<double>{0.0, 0.0, 0.0});
+//             this->declare_parameter("start_orientation", std::vector<double>{0.0, 0.0, 0.0, 1.0});
+//             velocity_subr_ = this->create_subscription<geometry_msgs::msg::Twist>("command_velocity", 10, std::bind(&Little_Robot::velocityCallback, this, std::placeholders::_1));
+//             std::vector<double> start_position, start_orientation;
+//             this->get_parameter("start_position", start_position);
+//             this->get_parameter("start_orientation", start_orientation);
+//             pose_.position.set__x(start_position[0]);
+//             pose_.position.set__y(start_position[1]);
+//             pose_.position.set__z(start_position[2]);
+//             pose_.orientation.set__x(start_orientation[0]);
+//             pose_.orientation.set__y(start_orientation[1]);
+//             pose_.orientation.set__z(start_orientation[2]);
+//             pose_.orientation.set__w(start_orientation[3]);
+//             RCLCPP_INFO(this->get_logger(), "POSE: Position: (x: %0.3f, y: %0.3f, z: %0.3f) ", start_position[0], start_position[1], start_position[2]);
+//             RCLCPP_INFO(this->get_logger(), "POSE: Orientation: (x: %0.3f, y: %0.3f, z: %0.3f, w: %0.3f) ", start_orientation[0], start_orientation[1], start_orientation[2], start_orientation[3]);
+//         }
+//     private:
+//         geometry_msgs::msg::Pose pose_;
+//         geometry_msgs::msg::Twist twist_;
+//         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr velocity_subr_;
+//         rclcpp_action::Server<little_bot::action::RotateAbsolute>::SharedPtr rotate_absolute_action_server_;
+//         std::shared_ptr<RotateAbsoluteGoalHandle> rotate_absolute_goal_handle_;
+//         std::shared_ptr<little_bot::action::RotateAbsolute::Feedback> rotate_absolute_feedback_;
+//         std::shared_ptr<little_bot::action::RotateAbsolute::Result> rotate_absolute_result_;
 
-        rclcpp::Time last_command_time_;
+//         rclcpp::Time last_command_time_;
 
-        void velocityCallback(const geometry_msgs::msg::Twist::SharedPtr velocity){
-            last_command_time_ = this->now();
-            twist_.set__linear(velocity->linear);
-            twist_.set__angular(velocity->angular);
-            // lin_vel_x_ = velocity->linear.x;
-            // lin_vel_y_ = velocity->linear.y;
-            // ang_vel_ = velocity->angular.z;
+//         void velocityCallback(const geometry_msgs::msg::Twist::SharedPtr velocity){
+//             last_command_time_ = this->now();
+//             twist_.set__linear(velocity->linear);
+//             twist_.set__angular(velocity->angular);
+//             // lin_vel_x_ = velocity->linear.x;
+//             // lin_vel_y_ = velocity->linear.y;
+//             // ang_vel_ = velocity->angular.z;
 
-            if (rotate_absolute_goal_handle_){
-                RCLCPP_WARN(this->get_logger(), "Velocity command received during rotation goal. Aborting goal");
-                rotate_absolute_goal_handle_->abort(rotate_absolute_result_);
-                rotate_absolute_goal_handle_ = nullptr;
-            }else{
-                RCLCPP_INFO(this->get_logger(), "TWIST: Linear: (x: %0.3f, y: %0.3f, z: %0.3f) ", twist_.linear.x, twist_.linear.y, twist_.linear.z);
-                RCLCPP_INFO(this->get_logger(), "TWIST: Angular: (x: %0.3f, y: %0.3f, z: %0.3f) ", twist_.angular.x , twist_.angular.y, twist_.angular.z);
-            }   
-        }
+//             if (rotate_absolute_goal_handle_){
+//                 RCLCPP_WARN(this->get_logger(), "Velocity command received during rotation goal. Aborting goal");
+//                 rotate_absolute_goal_handle_->abort(rotate_absolute_result_);
+//                 rotate_absolute_goal_handle_ = nullptr;
+//             }else{
+//                 RCLCPP_INFO(this->get_logger(), "TWIST: Linear: (x: %0.3f, y: %0.3f, z: %0.3f) ", twist_.linear.x, twist_.linear.y, twist_.linear.z);
+//                 RCLCPP_INFO(this->get_logger(), "TWIST: Angular: (x: %0.3f, y: %0.3f, z: %0.3f) ", twist_.angular.x , twist_.angular.y, twist_.angular.z);
+//             }   
+//         }
 
 
-        // void Turtle::rotateAbsoluteAcceptCallback(const std::shared_ptr<RotateAbsoluteGoalHandle> goal_handle){
-        //     // Abort any existing goal
-        //     if (rotate_absolute_goal_handle_){
-        //         RCLCPP_WARN(nh_->get_logger(), "Rotation goal received before a previous goal finished. Aborting previous goal");
-        //         rotate_absolute_goal_handle_->abort(rotate_absolute_result_);
-        //     }
-        //     rotate_absolute_goal_handle_ = goal_handle;
-        //     rotate_absolute_feedback_.reset(new turtlesim::action::RotateAbsolute::Feedback);
-        //     rotate_absolute_result_.reset(new turtlesim::action::RotateAbsolute::Result);
-        //     rotate_absolute_start_orient_ = orient_;
-        // }
-};
+//         // void Turtle::rotateAbsoluteAcceptCallback(const std::shared_ptr<RotateAbsoluteGoalHandle> goal_handle){
+//         //     // Abort any existing goal
+//         //     if (rotate_absolute_goal_handle_){
+//         //         RCLCPP_WARN(nh_->get_logger(), "Rotation goal received before a previous goal finished. Aborting previous goal");
+//         //         rotate_absolute_goal_handle_->abort(rotate_absolute_result_);
+//         //     }
+//         //     rotate_absolute_goal_handle_ = goal_handle;
+//         //     rotate_absolute_feedback_.reset(new turtlesim::action::RotateAbsolute::Feedback);
+//         //     rotate_absolute_result_.reset(new turtlesim::action::RotateAbsolute::Result);
+//         //     rotate_absolute_start_orient_ = orient_;
+//         // }
+// };
 
-int main(int argc, char * argv[]){
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<Little_Robot>());
-  rclcpp::shutdown();
-  return 0;
-}
+// int main(int argc, char * argv[]){
+//   rclcpp::init(argc, argv);
+//   rclcpp::spin(std::make_shared<Little_Robot>());
+//   rclcpp::shutdown();
+//   return 0;
+// }
--- src/remote_controller.cpp
+++ src/remote_controller.cpp
@@ -1,195 +1,195 @@
-/*---REMOTE_CONTROLLER_CPP---*/
-/*----------------------------------------------------------------------------*/
-/*    Module:       remote_controller.cpp                                     */
-/*    Author:       Jeffrey Fisher II                                         */
-/*    Modified:     2022-01-30                                                */
-/*----------------------------------------------------------------------------*/
+// /*---REMOTE_CONTROLLER_CPP---*/
+// /*----------------------------------------------------------------------------*/
+// /*    Module:       remote_controller.cpp                                     */
+// /*    Author:       Jeffrey Fisher II                                         */
+// /*    Modified:     2022-01-30                                                */
+// /*----------------------------------------------------------------------------*/
 
-/* HEADER INCLUDE */
-#include "little_bot/remote_controller.hpp"
+// /* HEADER INCLUDE */
+// #include "little_bot/remote_controller.hpp"
 
-/*----- CLASS Keyboard_Reader -----*/
-/* PUBLIC */
-Keyboard_Reader::Keyboard_Reader(): kfd(0){
-	// get the console in raw mode
-	tcgetattr(kfd, &cooked);
-	struct termios raw;
-	memcpy(&raw, &cooked, sizeof(struct termios));
-	raw.c_lflag &=~ (ICANON | ECHO);
-	// Setting a new line, then end of file
-	raw.c_cc[VEOL] = 1;
-	raw.c_cc[VEOF] = 2;
-	tcsetattr(kfd, TCSANOW, &raw);
-}
+// /*----- CLASS Keyboard_Reader -----*/
+// /* PUBLIC */
+// Keyboard_Reader::Keyboard_Reader(): kfd(0){
+// 	// get the console in raw mode
+// 	tcgetattr(kfd, &cooked);
+// 	struct termios raw;
+// 	memcpy(&raw, &cooked, sizeof(struct termios));
+// 	raw.c_lflag &=~ (ICANON | ECHO);
+// 	// Setting a new line, then end of file
+// 	raw.c_cc[VEOL] = 1;
+// 	raw.c_cc[VEOF] = 2;
+// 	tcsetattr(kfd, TCSANOW, &raw);
+// }
 
-void Keyboard_Reader::readOne(char * c){
-	int rc = read(kfd, c, 1);
-	if(rc < 0){ throw std::runtime_error("read failed"); }
-}
+// void Keyboard_Reader::readOne(char * c){
+// 	int rc = read(kfd, c, 1);
+// 	if(rc < 0){ throw std::runtime_error("read failed"); }
+// }
 		
-void Keyboard_Reader::shutdown(){
-	tcsetattr(kfd, TCSANOW, &cooked);
-}
+// void Keyboard_Reader::shutdown(){
+// 	tcsetattr(kfd, TCSANOW, &cooked);
+// }
 
 
-/*----- CLASS Remote_Controller -----*/
-/* PUBLIC */
-Remote_Controller::Remote_Controller(): linear_(0), angular_(0), l_scale_(2.0), a_scale_(2.0){
-	node_ = rclcpp::Node::make_shared("remote_controller");
-	node_->declare_parameter("scale_angular", rclcpp::ParameterValue(2.0));
-	node_->declare_parameter("scale_linear", rclcpp::ParameterValue(2.0));
-	node_->get_parameter("scale_angular", a_scale_);
-	node_->get_parameter("scale_linear", l_scale_);
+// /*----- CLASS Remote_Controller -----*/
+// /* PUBLIC */
+// Remote_Controller::Remote_Controller(): linear_(0), angular_(0), l_scale_(2.0), a_scale_(2.0){
+// 	node_ = rclcpp::Node::make_shared("remote_controller");
+// 	node_->declare_parameter("scale_angular", rclcpp::ParameterValue(2.0));
+// 	node_->declare_parameter("scale_linear", rclcpp::ParameterValue(2.0));
+// 	node_->get_parameter("scale_angular", a_scale_);
+// 	node_->get_parameter("scale_linear", l_scale_);
 
-	twist_pub_ = node_->create_publisher<geometry_msgs::msg::Twist>("command_velocity", 1);
-	rotate_absolute_client_ = rclcpp_action::create_client<little_bot::action::RotateAbsolute>(node_, "rotate_absolute");
-}
+// 	twist_pub_ = node_->create_publisher<geometry_msgs::msg::Twist>("command_velocity", 1);
+// 	rotate_absolute_client_ = rclcpp_action::create_client<little_bot::action::RotateAbsolute>(node_, "rotate_absolute");
+// }
 
-int Remote_Controller::keyLoop(){
-	char c;
-	bool dirty=false;
-	std::thread{std::bind(&Remote_Controller::spin, this)}.detach();
-	puts("Reading from keyboard");
-	puts("---------------------------");
-	puts("Use arrow keys to move the little_bot.");
-	puts("Use G|B|V|C|D|E|R|T keys to rotate to absolute orientations. 'F' to cancel a rotation.");
-	puts("'Q' to quit.");
+// int Remote_Controller::keyLoop(){
+// 	char c;
+// 	bool dirty=false;
+// 	std::thread{std::bind(&Remote_Controller::spin, this)}.detach();
+// 	puts("Reading from keyboard");
+// 	puts("---------------------------");
+// 	puts("Use arrow keys to move the little_bot.");
+// 	puts("Use G|B|V|C|D|E|R|T keys to rotate to absolute orientations. 'F' to cancel a rotation.");
+// 	puts("'Q' to quit.");
 
-	for(;;){
-		// get the next event from the keyboard  
-		try{
-			input.readOne(&c);
-		}catch(const std::runtime_error &){
-			perror("read():");
-			return -1;
-		}
+// 	for(;;){
+// 		// get the next event from the keyboard  
+// 		try{
+// 			input.readOne(&c);
+// 		}catch(const std::runtime_error &){
+// 			perror("read():");
+// 			return -1;
+// 		}
 
-		linear_=angular_=0;
-		RCLCPP_DEBUG(node_->get_logger(), "value: 0x%02X\n", c);
+// 		linear_=angular_=0;
+// 		RCLCPP_DEBUG(node_->get_logger(), "value: 0x%02X\n", c);
   
-		switch(c){
-			case KEYCODE_LEFT:
-				RCLCPP_DEBUG(node_->get_logger(), "LEFT");
-				angular_ = 1.0;
-				dirty = true;
-				break;
-			case KEYCODE_RIGHT:
-				RCLCPP_DEBUG(node_->get_logger(), "RIGHT");
-				angular_ = -1.0;
-				dirty = true;
-				break;
-			case KEYCODE_UP:
-				RCLCPP_DEBUG(node_->get_logger(), "UP");
-				linear_ = 1.0;
-				dirty = true;
-				break;
-			case KEYCODE_DOWN:
-				RCLCPP_DEBUG(node_->get_logger(), "DOWN");
-				linear_ = -1.0;
-				dirty = true;
-				break;
-			case KEYCODE_G:
-				RCLCPP_DEBUG(node_->get_logger(), "G");
-				sendGoal(0.0f);
-				break;
-			case KEYCODE_T:
-				RCLCPP_DEBUG(node_->get_logger(), "T");
-				sendGoal(0.7854f);
-				break;
-			case KEYCODE_R:
-				RCLCPP_DEBUG(node_->get_logger(), "R");
-				sendGoal(1.5708f);
-				break;
-			case KEYCODE_E:
-				RCLCPP_DEBUG(node_->get_logger(), "E");
-				sendGoal(2.3562f);
-				break;
-			case KEYCODE_D:
-				RCLCPP_DEBUG(node_->get_logger(), "D");
-				sendGoal(3.1416f);
-				break;
-			case KEYCODE_C:
-				RCLCPP_DEBUG(node_->get_logger(), "C");
-				sendGoal(-2.3562f);
-				break;
-			case KEYCODE_V:
-				RCLCPP_DEBUG(node_->get_logger(), "V");
-				sendGoal(-1.5708f);
-				break;
-			case KEYCODE_B:
-				RCLCPP_DEBUG(node_->get_logger(), "B");
-				sendGoal(-0.7854f);
-				break;
-			case KEYCODE_F:
-				RCLCPP_DEBUG(node_->get_logger(), "F");
-				cancelGoal();
-				break;
-			case KEYCODE_Q:
-				RCLCPP_DEBUG(node_->get_logger(), "quit");
-				return 0;
-		}
+// 		switch(c){
+// 			case KEYCODE_LEFT:
+// 				RCLCPP_DEBUG(node_->get_logger(), "LEFT");
+// 				angular_ = 1.0;
+// 				dirty = true;
+// 				break;
+// 			case KEYCODE_RIGHT:
+// 				RCLCPP_DEBUG(node_->get_logger(), "RIGHT");
+// 				angular_ = -1.0;
+// 				dirty = true;
+// 				break;
+// 			case KEYCODE_UP:
+// 				RCLCPP_DEBUG(node_->get_logger(), "UP");
+// 				linear_ = 1.0;
+// 				dirty = true;
+// 				break;
+// 			case KEYCODE_DOWN:
+// 				RCLCPP_DEBUG(node_->get_logger(), "DOWN");
+// 				linear_ = -1.0;
+// 				dirty = true;
+// 				break;
+// 			case KEYCODE_G:
+// 				RCLCPP_DEBUG(node_->get_logger(), "G");
+// 				sendGoal(0.0f);
+// 				break;
+// 			case KEYCODE_T:
+// 				RCLCPP_DEBUG(node_->get_logger(), "T");
+// 				sendGoal(0.7854f);
+// 				break;
+// 			case KEYCODE_R:
+// 				RCLCPP_DEBUG(node_->get_logger(), "R");
+// 				sendGoal(1.5708f);
+// 				break;
+// 			case KEYCODE_E:
+// 				RCLCPP_DEBUG(node_->get_logger(), "E");
+// 				sendGoal(2.3562f);
+// 				break;
+// 			case KEYCODE_D:
+// 				RCLCPP_DEBUG(node_->get_logger(), "D");
+// 				sendGoal(3.1416f);
+// 				break;
+// 			case KEYCODE_C:
+// 				RCLCPP_DEBUG(node_->get_logger(), "C");
+// 				sendGoal(-2.3562f);
+// 				break;
+// 			case KEYCODE_V:
+// 				RCLCPP_DEBUG(node_->get_logger(), "V");
+// 				sendGoal(-1.5708f);
+// 				break;
+// 			case KEYCODE_B:
+// 				RCLCPP_DEBUG(node_->get_logger(), "B");
+// 				sendGoal(-0.7854f);
+// 				break;
+// 			case KEYCODE_F:
+// 				RCLCPP_DEBUG(node_->get_logger(), "F");
+// 				cancelGoal();
+// 				break;
+// 			case KEYCODE_Q:
+// 				RCLCPP_DEBUG(node_->get_logger(), "quit");
+// 				return 0;
+// 		}
 
-    	geometry_msgs::msg::Twist twist;
-    	twist.angular.z = a_scale_*angular_;
-    	twist.linear.x = l_scale_*linear_;
-    	if(dirty ==true){
-      		twist_pub_->publish(twist);
-      		dirty=false;
-    	}
-  	}
-	return 0;
-}
+//     	geometry_msgs::msg::Twist twist;
+//     	twist.angular.z = a_scale_*angular_;
+//     	twist.linear.x = l_scale_*linear_;
+//     	if(dirty ==true){
+//       		twist_pub_->publish(twist);
+//       		dirty=false;
+//     	}
+//   	}
+// 	return 0;
+// }
 
-/* PRIVATE */
-void Remote_Controller::spin(){
-	while (rclcpp::ok()){
-		rclcpp::spin_some(node_);
-	}
-}
+// /* PRIVATE */
+// void Remote_Controller::spin(){
+// 	while (rclcpp::ok()){
+// 		rclcpp::spin_some(node_);
+// 	}
+// }
 
-void Remote_Controller::sendGoal(float theta){
-	auto goal = little_bot::action::RotateAbsolute::Goal();
-	goal.theta = theta;
-	auto send_goal_options = rclcpp_action::Client<little_bot::action::RotateAbsolute>::SendGoalOptions();
-	send_goal_options.goal_response_callback = [this](std::shared_future<rclcpp_action::ClientGoalHandle<little_bot::action::RotateAbsolute>::SharedPtr> future){
-		RCLCPP_DEBUG(node_->get_logger(), "Goal response received");
-		this->goal_handle_ = future.get();
-	};
-	rotate_absolute_client_->async_send_goal(goal, send_goal_options);
-}
+// void Remote_Controller::sendGoal(float theta){
+// 	auto goal = little_bot::action::RotateAbsolute::Goal();
+// 	goal.theta = theta;
+// 	auto send_goal_options = rclcpp_action::Client<little_bot::action::RotateAbsolute>::SendGoalOptions();
+// 	send_goal_options.goal_response_callback = [this](std::shared_future<rclcpp_action::ClientGoalHandle<little_bot::action::RotateAbsolute>::SharedPtr> future){
+// 		RCLCPP_DEBUG(node_->get_logger(), "Goal response received");
+// 		this->goal_handle_ = future.get();
+// 	};
+// 	rotate_absolute_client_->async_send_goal(goal, send_goal_options);
+// }
 
-void Remote_Controller::goalResponseCallback(std::shared_future<rclcpp_action::ClientGoalHandle<little_bot::action::RotateAbsolute>::SharedPtr> future){
-	RCLCPP_DEBUG(node_->get_logger(), "Goal response received");
-	this->goal_handle_ = future.get();
-}
+// void Remote_Controller::goalResponseCallback(std::shared_future<rclcpp_action::ClientGoalHandle<little_bot::action::RotateAbsolute>::SharedPtr> future){
+// 	RCLCPP_DEBUG(node_->get_logger(), "Goal response received");
+// 	this->goal_handle_ = future.get();
+// }
 
-void Remote_Controller::cancelGoal(){
-	if(goal_handle_){
-		RCLCPP_DEBUG(node_->get_logger(), "Sending cancel request");
-		try{
-			rotate_absolute_client_->async_cancel_goal(goal_handle_);
-		}catch(...){
-			// This can happen if the goal has already terminated and expired
-		}
-	}
-}
+// void Remote_Controller::cancelGoal(){
+// 	if(goal_handle_){
+// 		RCLCPP_DEBUG(node_->get_logger(), "Sending cancel request");
+// 		try{
+// 			rotate_absolute_client_->async_cancel_goal(goal_handle_);
+// 		}catch(...){
+// 			// This can happen if the goal has already terminated and expired
+// 		}
+// 	}
+// }
 
 
-/*----- MAIN -----*/
-void quit(int sig){
-	(void)sig;
-	input.shutdown();
-	rclcpp::shutdown();
-	exit(0);
-}
+// /*----- MAIN -----*/
+// void quit(int sig){
+// 	(void)sig;
+// 	input.shutdown();
+// 	rclcpp::shutdown();
+// 	exit(0);
+// }
 
-int main(int argc, char** argv){
-	rclcpp::init(argc, argv);
-	Remote_Controller remote_controller;
-	signal(SIGINT,quit);
-	int rc = remote_controller.keyLoop();
-	input.shutdown();
-	rclcpp::shutdown();
-	return rc;
-}
-/*---REMOTE_CONTROLLER_CPP---*/
+// int main(int argc, char** argv){
+// 	rclcpp::init(argc, argv);
+// 	Remote_Controller remote_controller;
+// 	signal(SIGINT,quit);
+// 	int rc = remote_controller.keyLoop();
+// 	input.shutdown();
+// 	rclcpp::shutdown();
+// 	return rc;
+// }
+// /*---REMOTE_CONTROLLER_CPP---*/
