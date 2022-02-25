7ef65c26162dd1327b842a5eb098a9173f8f49a9
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2022-02-24 21:29:27 -0800

    2D Sim Package Setup

---
+++ 2d_lilbot_sim/2d_lilbot_sim/2d_lilbot_sim.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from 2d_lilbot_sim.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2d_lilbot_sim/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = '2d_lilbot_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jeffrey',
+    maintainer_email='jeffreyspaceboy@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            '2d_lilbot_sim = 2d_lilbot_sim.2d_lilbot_sim:main'
+        ],
+    },
+)
