864bd54a4c25f19f345c33e6ee5317799ec838e2
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2022-02-27 11:51:57 -0800

    URDF basic example working

---
+++ little_bot_3d_sim/launch/display.launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file_name = 'urdf/little_bot_3d_sim.urdf.xml'
+    urdf = os.path.join(
+        get_package_share_directory('little_bot_3d_sim'),
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf]),
+        Node(
+            package='little_bot_3d_sim',
+            executable='state_publisher',
+            name='state_publisher',
+            output='screen'),
+    ])
