8bee56689787f2a6d99f9082262dc3dbb15b6e04
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2022-03-02 22:08:53 -0800

    Pre-ROS Integration

--- lilbot_webot_sim/src/lilbot_webot_sim.cpp
+++ lilbot_webot_sim/src/lilbot_webot_sim.cpp
@@ -1,4 +1,17 @@
-#include "lilbot_webot_sim/lilbot_webot_sim.hpp"
+#include "rclcpp/macros.hpp"
+#include "webots_ros2_driver/PluginInterface.hpp"
+#include "webots_ros2_driver/WebotsNode.hpp"
+
+namespace webots_ros2_plugin_example
+{
+	class WebotsRos2PluginExample : public webots_ros2_driver::PluginInterface
+	{
+		public:
+		// Your plugin has to override step() and init() methods
+		void step() override;
+		void init(webots_ros2_driver::WebotsNode *node, std::unordered_map<std::string, std::string> &parameters) override;
+	};
+}
 
 namespace webots_ros2_plugin_example
 {
